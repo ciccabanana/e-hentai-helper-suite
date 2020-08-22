f56985b6aa6cf00af996e772faefc3e23deb363a
ciccabanana <ciccabanana@hotmail.it>
2020-08-22 20:11:17 +0200

    Script optimization, update README and library

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# e-hentai-helper-suite-
-Suite of script for bettern navigation on e-hentai
+# e-hentai-helper-suite
+
+Collection of script for a better navigation on e-hentai
+
+## e-hentai-tags-helper
+
+<p align="center">
+  <img src="docs/tag-helper.gif" />
+</p>
+
+Replace normal search bar with new one whit autocomplete of tags
+
+### Features
+* Compatible with Tampermonkey, Greasemonkey and Violentmonkey
+* Fast loading ~40ms
+* Autocomplete of tags using site API
+* More fast autocomplete using category index [see below](#Category-index)
+* Colored tags by category &nbsp;
+![](https://img.shields.io/static/v1?label=&message=female&color=f75e56)&nbsp;
+![](https://img.shields.io/static/v1?label=&message=male&color=374eb3)&nbsp;
+![](https://img.shields.io/static/v1?label=&message=language&color=0eac10)&nbsp;
+![](https://img.shields.io/static/v1?label=&message=parody&color=902cdd)&nbsp;
+![](https://img.shields.io/static/v1?label=&message=character&color=db75d5)&nbsp;
+![](https://img.shields.io/static/v1?label=&message=group&color=f09e19)&nbsp;
+![](https://img.shields.io/static/v1?label=&message=artist&color=d3d303)&nbsp;
+![](https://img.shields.io/static/v1?label=&message=misc&color=808080)
+
+#### Category index
+- f: => female
+- m: => male
+- l: => language
+- p: => parody
+- c: => character
+- g: => group
+- a: => artist
+- x: OR mi: => misc
+
+### ToDo
+- [ ] 
+- [ ] Settings interface
+- [ ] Possibility of user to personalize tags color
+
+## External Tools
+
+- [tagify](https://github.com/yairEO/tagify)
+- [@saninn/logger](https://github.com/distante/saninn-logger)
---
+++ e-hentai-tags-helper.user.js
@@ -0,0 +1,393 @@
+// ==UserScript==
+// @name Tags Auto Complete
+// @namespace https://github.com/ciccabanana/e-hentai-helper-suite
+// @homepageURL https://github.com/ciccabanana/e-hentai-helper-suite
+// @version 0.1
+// @encoding utf-8
+// @author      ciccabanana
+// @description     Replace normal search bar with new one whit autocomplete of tags
+// @supportURL https://github.com/ciccabanana/e-hentai-helper-suite/issues
+// @updateURL   https://raw.githubusercontent.com/ciccabanana/e-hentai-helper-suite/develop/e-hentai-tags-helper.js
+// @include     /https?:\/\/e(-|x)hentai\.org\/(watched.*|tag\/.*|\?f_.*|)/
+// @eexclude
+
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require https://raw.githubusercontent.com/ciccabanana/e-hentai-helper-suite/develop/@saninn__logger.js
+// @require https://raw.githubusercontent.com/ciccabanana/e-hentai-helper-suite/develop/jQuery.tagify.min.js
+
+// @resource    TagifyCSS https://raw.githubusercontent.com/ciccabanana/e-hentai-helper-suite/develop/tagify.css
+
+// @grant   GM_getResourceText
+// @grant   GM_info
+
+// @grant   GM.getResourceUrl
+// @grant   GM.info
+// @grant   GM.xmlHttpRequest
+
+// ==/UserScript==
+
+
+var debug = true;
+
+console.time();
+(function () {
+    'use strict';
+
+    var makeXMLRequest = function (url, method = "GET", body = null) {
+        var request = new XMLHttpRequest();
+
+        // Return it as a Promise
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+            // Setup our listener to process compeleted requests
+            request.onreadystatechange = function () {
+                // Only run if the request is complete
+                if (request.readyState !== 4) return;
+
+                // Process the response
+                if (request.status >= 200 && request.status < 300) {
+                    // If successful
+                    resolve(request);
+                } else {
+                    // If failed
+                    reject({
+                        status: request.status,
+                        statusText: request.statusText
+                    });
+                }
+
+            };
+
+            // Setup our HTTP request
+            request.open(method, url, true);
+            //request.setRequestHeader("Content-Type", "application/json");
+            //request.withCredentials = true;
+
+            // Send the request
+            request.send(body);
+            }, 200);
+        });
+    };
+
+    var getResourceText = function (resource, func) {
+        if (typeof (GM_getResourceText) !== 'undefined') {
+            // Tampermonkey and Violentmonkey
+            func(GM_getResourceText(resource))
+        } else if (typeof (GM.getResourceUrl) !== 'undefined') {
+            // Greasemonkey and Tampermonkey (If TM compatibility is on)
+            GM.getResourceUrl(resource).then(function (blobURL) {
+
+                makeXMLRequest(blobURL).then(function (result) {
+                    func(result.responseText);
+                }).catch((reason) => {
+                    mConsole.log('Reasion: ', reason);
+                });
+            }).catch((reason) => {
+                mConsole.log('Reasion: ', reason);
+            });
+        }
+    };
+
+    // Create custom mConsole
+    var mConsole = new SaninnLogger('Tags Auto');
+
+    getResourceText("TagifyCSS", addStyle);
+
+    // Print curent wesite
+    mConsole.log("Site api: ", location.hostname);
+    mConsole.log(location.pathname);
+
+    if (location.hostname == "e-hentai.org") {
+        var api_url = "https://api.e-hentai.org/api.php";
+        var set_tag_color = set_tag_color_e;
+        addStyle(`.tagify {
+            background-color: #edeada;
+            --tags-border-color: #B5A4A4;
+        }
+        .tagify:hover {
+            background-color: #F3F0E0;
+            border-color: #B5A4A4
+        }
+        .tagify__input::before {
+            color: #5C0D12;
+        }
+        .tagify__dropdown__wrapper {
+            background: #EDEBDF;
+            border: 1px solid #D4D4D4;
+        }
+        .tagify__dropdown__item--active {
+            background: #F3F0E0!important;
+            color: #5C0D11
+        }`);
+    } else {
+
+        api_url = "https://exhentai.org/api.php";
+        var set_tag_color = set_tag_color_ex;
+
+        addStyle(`.tagify {
+            background-color: #34353b;
+            --tags-border-color: #8d8d8d;
+        }
+        .tagify:hover {
+            background-color: #43464e;
+            border-color: #8d8d8d
+        }
+        .tagify__input::before {
+            color: #f1f1f1;
+        }
+        .tagify__dropdown__wrapper {
+            background: #4f535b;
+            /*bakgound della lista*/
+            border: 1px solid #8d8d8d;
+        }
+        .tagify__dropdown__item--active {
+            background: #43464e!important;
+            color: #f1f1f1
+        }`);
+    }
+
+    // Hide the original search bar
+    $("#searchbox > form > p:nth-child(3)").attr('style', 'display:none;');
+
+    // Create new container
+    var container = document.createElement("DIV");
+    container.setAttribute("class", "tagcomplete");
+
+    // Create new input text
+    var tag_bar = document.createElement("input");
+    tag_bar.setAttribute("type", "text");
+    tag_bar.setAttribute("id", "tagname_new_test");
+    tag_bar.setAttribute("name", "tagname_new_test");
+    tag_bar.setAttribute("placeholder", "Insert tags ");
+    tag_bar.setAttribute("autofocus", '');
+
+    // Append 
+    container.appendChild(tag_bar);
+    $("#searchbox > form > p:nth-child(3)").after(container);
+
+    // Clone and append button "Clear Filter"
+    $("#searchbox > form > p:nth-child(3) > input[type=button]:nth-child(3)").clone().insertAfter("#searchbox > form > div.tagcomplete");
+    // Clone and append button "Apply Filter"
+    $("#searchbox > form > p:nth-child(3) > input[type=submit]:nth-child(2)").clone().insertAfter("#searchbox > form > div.tagcomplete");
+
+
+    // Create tagify bar
+    var tagify = new Tagify(tag_bar, {
+        transformTag: set_tag_color,
+        templates: {
+            tag: function (tagData) {
+                try {
+                    tagData.value = tagData.value.replace(/@|#/g, '');
+                    return `<tag title='${tagData.value}' contenteditable='false' spellcheck="false" class='tagify__tag ${tagData.class ? tagData.class : ""}' ${this.getAttributes(tagData)}>
+							<x title='' class='tagify__tag__removeBtn' role='button' aria-label='remove tag'></x>
+							<div>
+								<span class='tagify__tag-text'>${tagData.value}</span>
+							</div>
+						</tag>`;
+                } catch (err) { }
+            },
+            dropdownItem: function (tagData) {
+                try {
+                    var value = tagData.value.split('#').map(element => element[0] == "@" ? "<strong>" + element.substring(1) + "</strong>" : element).join("");
+                    tagData.value = tagData.value.replace(/@|#/g, '');
+                    var html = `<div ${this.getAttributes(tagData)} 
+                        class='${this.settings.classNames.dropdownItem} ${tagData.class ? tagData.class : ""}'
+                        tabindex=\"0\" role=\"option\">
+                            ${value}
+						</div>`;
+                    return html;
+                } catch (err) { }
+            }
+        }
+    });
+
+    // Get current search value
+    var old_input = $("#f_search")[0].value.match(/\w+:(?:\")?(?:[^\$\"]*)\$(?:\")?|(?:[^ ])(?:\")?(?:[\w\s]*)\$(?:\")?|\"(?:[^\"]*)\"/g);
+    if (old_input) {
+        old_input = old_input.map(function (item, index) {
+            return {
+                value: item.replace(/["\'\$]/g, ''),
+                detail: item.match(/^.*:.*$/g) ? item : NaN,
+                editable: item.match(/^.*:.*$/g) ? false : true
+            };
+        });
+        tagify.addTags(old_input);
+    }
+
+    // Set event listeners
+    tagify.on('add', onAddTag).on('remove', onRemoveTag).on('input', onInput).on('dropdown:select', onDropdownSelect).on('change', onChange);
+
+    // tag added callback
+    function onAddTag(e) {
+        if (debug)
+            mConsole.log("onAddTag: ", e.detail.data);
+    }
+
+    // tag remvoed callback
+    function onRemoveTag(e) {
+        if (debug)
+            mConsole.log("onRemoveTag:", e.detail.data);
+    }
+
+    // on character(s) added/removed (user is typing/deleting)
+    function onInput(e) {
+        if (debug)
+            mConsole.log("onInput: ", e.detail);
+
+        tagify.settings.whitelist.length = 0; // reset current whitelist
+
+        regex_replace(e.detail.value).then(function (pre_elab_result) {
+            // show the loader animation
+            tagify.loading(true).dropdown.hide.call(tagify);
+
+            makeXMLRequest(api_url, "POST", JSON.stringify({
+                method: "tagsuggest",
+                text: pre_elab_result
+            }))
+                .then(function (result) {
+
+                    result = JSON.parse(result.responseText);
+                    var p = new RegExp("(^| |:)" + pre_elab_result, "ig");
+                    var a = Object.values(result.tags).map(function (key) {
+                        return {
+                            value: (key.ns + ":" + key.tn).match(p) ? (key.ns + ":" + key.tn).replace(p, "#@$&#") : key.ns + ":" + key.tn,
+                            detail: key.tn.indexOf(" ") != -1 ? key.ns + ":\"" + key.tn + "$\"" : key.ns + ":" + key.tn + "$",
+                            editable: false
+                        };
+                    });
+
+                    // Add element in whitelist
+                    tagify.settings.whitelist.splice(0, a.length, ...a);
+
+                    // render the suggestions dropdown.
+                    tagify.loading(false).dropdown.show.call(tagify, pre_elab_result);
+
+                }).catch((reason) => {
+                    mConsole.log('Server request failed.\nStatus: ', reason.status, '\nResponse: ', reason.statusText);
+                });
+        }).catch((reason) => {
+            if (debug)
+                mConsole.log('Pre-request elab failed. Reasion: ', reason);
+        });
+    }
+
+    function onDropdownSelect(e) {
+        if (debug)
+            mConsole.log("onDropdownSelect: ", e.detail);
+    }
+
+    function onChange(e) {
+        // outputs a String
+        if (debug)
+            mConsole.log("onChange: ", e.detail);
+        var text = "";
+        if (e.detail.value) {
+            var list = JSON.parse(e.detail.value);
+            list = list.map(x => x.detail ? x.detail : `"${x.value}"`); // estraggo i dettagli
+            text = list.join(" ");
+        }
+
+        $("#f_search")[0].value = text;
+    }
+
+    function set_tag_color_e(tagData) {
+
+        var trasform = tagData.value.split(":")[0].replace(/@|#/g, '');
+        switch (trasform) {
+            case 'female':
+                tagData.style = "--tag-bg:" + '#f75e56'; //  (Red) (Doujinshi)
+                break;
+            case 'male':
+                tagData.style = "--tag-bg:" + '#374eb3'; //  (Blue) (Image Set)
+                break;
+            case 'reclass':
+                //    tagData.style = "--tag-bg:" + '#ab9f60'; // Teak (Western)          
+                break;
+            case 'language':
+                tagData.style = "--tag-bg:" + '#0eac10'; //  (Green) (Game CG)
+                break;
+            case 'parody':
+                tagData.style = "--tag-bg:" + '#902cdd'; // #8f2adb (Cosplay)
+                break;
+            case 'character':
+                tagData.style = "--tag-bg:" + '#db75d5'; //  (Viola) (Asian)
+                break;
+            case 'group':
+                tagData.style = "--tag-bg:" + '#f09e19'; // (Oragne) (Manga)
+                break;
+            case 'artist':
+                tagData.style = "--tag-bg:" + '#d3d303'; //  (YelloW) (Artistig CG)
+                break;
+            default:
+                tagData.style = "--tag-bg:" + '#808080'; //  (Gray) (Misc)
+                break;
+        }
+    }
+
+    function set_tag_color_ex(tagData) {
+
+        var trasform = tagData.value.split(":")[0].replace(/@|#/g, '');
+        switch (trasform) {
+            case 'female':
+                tagData.style = "--tag-bg:" + '#940000'; // Red Berry (Red) (Doujinshi)
+                break;
+            case 'male':
+                tagData.style = "--tag-bg:" + '#325ca2'; // Azure (Blue) (Image Set)
+                break;
+            case 'reclass':
+                //    tagData.style = "--tag-bg:" + '#ab9f60'; // Teak (Western)          
+                break;
+            case 'language':
+                tagData.style = "--tag-bg:" + '#6a936d'; // Laurel (Green) (Game CG)
+                break;
+            case 'parody':
+                tagData.style = "--tag-bg:" + '#6a32a2'; // Royal Purple (Cosplay)
+                break;
+            case 'character':
+                tagData.style = "--tag-bg:" + '#a23282'; // Royal Heath (Viola) (Asian)
+                break;
+            case 'group':
+                tagData.style = "--tag-bg:" + '#db6c24'; // Hot Cinnamon (Oragne) (Manga)
+                break;
+            case 'artist':
+                tagData.style = "--tag-bg:" + '#d38f1d'; // Geebung (YelloW) (Artistig CG)
+                break;
+            default:
+                tagData.style = "--tag-bg:" + '#777777'; // Boulder (Gray) (Misc)
+                break;
+        }
+    }
+
+    function regex_replace(inp_text) {
+        return new Promise(function (resolve, reject) {
+            // ^(x|mi|f|m|r|l|p|c|g|a).*:
+            var reg = inp_text.replace(/["\']/g, "");
+
+            if (reg.match(/^(x|mi).*:/)) {
+                reg = reg.replace(/^(x|mi).*:/, "misc:");
+            } else {
+                reg = reg.replace(/^f.*:/, "female:").replace(/^m.*:/, "male:").replace(/^r.*:/, "reclass:").replace(/^l.*:/, "language:").replace(/^p.*:/, "parody:").replace(/^c.*:/, "character:").replace(/^g.*:/, "group:").replace(/^a.*:/, "artist:");
+            }
+            if (reg.replace(/^.*:/, "").length < 2) {
+                reject("length < 2");
+            } else {
+                resolve(reg);
+            }
+        });
+    }
+
+    function addStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        //if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.textContent = css;
+        head.appendChild(style);
+        return style; //optional, but convenient for changing the styling later.
+    };
+
+    // Your code here...
+})();
+
+console.timeEnd();
