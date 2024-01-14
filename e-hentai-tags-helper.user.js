3bdcdb055b9ebf82a7cccb8603fc4cdfdade15da
ciccabanana <ciccabanana@hotmail.it>
2024-01-14 17:32:18 +0100

    Some support comments, log and time

--- e-hentai-tags-helper.user.js
+++ e-hentai-tags-helper.user.js
@@ -213,6 +213,9 @@ if (userSettings.debugConsole) console.time('[Tags Auto Complete]: Loading time'
 (async function () {
     'use strict';
 
+    mConsole.log('Start Injection');
+    if (userSettings.debugConsole) console.time('[Tags Auto Complete]: Inject time');
+
     let open_settinga = async () => {
         var container = $(`
         <div class="tac-overlay">
@@ -627,6 +630,7 @@ if (userSettings.debugConsole) console.time('[Tags Auto Complete]: Loading time'
      * @param {string} resurce
      * @param {addStyle} callback
      */
+    // prettier-ignore
     const getResourceText = async (resurce, callback) => {
         if (typeof GM_getResourceText !== 'undefined') {
             // Tampermonkey and Violentmonkey
@@ -1000,6 +1004,7 @@ if (userSettings.debugConsole) console.time('[Tags Auto Complete]: Loading time'
     iconSvg.setAttribute('viewBox', '0 -960 960 960');
     iconSvg.setAttribute('height', '24');
     iconSvg.setAttribute('width', '24');
+    // prettier-ignore
     iconPath.setAttribute('d', 'm370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z');
     iconSvg.appendChild(iconPath);
 
@@ -1014,9 +1019,12 @@ if (userSettings.debugConsole) console.time('[Tags Auto Complete]: Loading time'
     let selector = location.pathname == '/favorites.php' ? 'body > div.ido > div:nth-child(3) > form > div' : '#searchbox > form > div:nth-child(3)';
 
     mConsole.log('Preload complete. Waintg for website...');
+    if (userSettings.debugConsole) console.timeLog('[Tags Auto Complete]: Inject time', 'Waintg for website...');
     // Wait the element before assembly all the things
     await waitForElement(selector);
     mConsole.log('Website loded');
+
+    if (userSettings.debugConsole) console.timeLog('[Tags Auto Complete]: Inject time', 'Website loded');
 
     // Append all the style at the head
     getResourceText('TagifyCSS', addStyle);
@@ -1102,6 +1110,10 @@ if (userSettings.debugConsole) console.time('[Tags Auto Complete]: Loading time'
         .on('click', onClick)
         .on('dblclick', ondblClick)
         .on('edit:start', onEditStart);
+
+    mConsole.log('Ended Injection');
+    if (userSettings.debugConsole) console.timeEnd('[Tags Auto Complete]: Inject time', 'Website loded');
+
 
     // Your code here...
 })();
